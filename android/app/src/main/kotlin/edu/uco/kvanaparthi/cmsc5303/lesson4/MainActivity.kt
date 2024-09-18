cfcf5e8613188ee010b0c049aec01f50a83becfa
Keerthi0912 <kvanaparthi@uco.edu>
2024-09-18 13:50:23 -0500

    intial commit

---
+++ android/app/src/main/kotlin/edu/uco/kvanaparthi/cmsc5303/lesson4/MainActivity.kt
@@ -0,0 +1,5 @@
+package edu.uco.kvanaparthi.cmsc5303.lesson4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/view/start_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatefulWidget{
+  const StartScreen({super.key});
+
+  static const routeName = '/startScreen';
+
+  @override
+  State<StatefulWidget> createState() {
+    return StartState();
+    
+  }
+
+}
+
+class StartState extends State<StartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Start Screen'),
+      ),
+      body: const Text('Start Screen body'),
+    );
+  }
+
+}
